b5f9d2d86654cbd6a713154b077b7fb442eae828
SeekerDev <albertbaisa@gmail.com>
2024-06-17 17:33:47 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Sidebar from "./layout/sidebar/Sidebar";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { isMobile } from "react-device-detect";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <div className="w-screen h-screen bg-slate-100 flex overflow-hidden MainBgColor">
       <Sidebar isMobile={isMobile} />
-
+      <Analytics />
       <Mainlayout isMobile={isMobile} />
       <ToastContainer />
     </div>
